e0a8781a11d680c879af09d1e5f48eca42fc3d6b
qwerty-arun <arunkr.1804@gmail.com>
2025-07-09 19:33:27 +0530

    strings: core concepts, indexing, slicing and encoding

--- 02_Data_types/chapter_6.py
+++ 02_Data_types/chapter_6.py
@@ -1,1 +1,20 @@
 # Strings
+chai_type = "Ginger Chai"
+customer_name = "Arun"
+
+print(f"Order for {customer_name} : {chai_type} please!")
+
+# Slicing and indexing
+chai_description = "Aromatic and Bold"
+print(f"First word: {chai_description[0:8]}")
+print(f"First word, every other word: {chai_description[0:8:2]}") # you can avoid 0
+print(f"Last word: {chai_description[12:]}")
+print(f"Last Word: {chai_description[::-1]}")
+
+# Encoding
+lable_text = "Chai Special"
+encoded_label = lable_text.encode("utf-8")
+print(f"Non-Encoded Label: {lable_text}")
+print(f"Encoded Label: {encoded_label}")
+decoded_label = encoded_label.decode("utf-8")
+print(f"Decoded Label: {decoded_label}")
