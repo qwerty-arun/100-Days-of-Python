34c171885d299706553c5a03fd8d9ba9c35e366e
qwerty-arun <arunkr.1804@gmail.com>
2025-08-04 17:58:59 +0530

    Learnt 3 projects: emoji enhancer, daily logger, age in minutes

--- Projects/Utilities/test.py
+++ Projects/Utilities/test.py
@@ -1,36 +1,24 @@
-import textwrap
+# emoji enhancer: take a sentence, add emoji after keywords
 
-name = input("Enter your name: ").strip()
-profession = input("Enter your profession: ").strip()
-passion = input("Enter your passion in one line: ").strip()
-emoji = input("Enter your favourite emoji: ").strip()
-website = input("Enter your website: ").strip()
+# get a dictionary
+emoji_map_fun ={
+    "love": "💖",
+    "happy": "😊",
+    "code": "💻",
+}
+# get user message
+message = input("Enter your message: ")
 
-print("\nChoose your style: ")
-print("1. Simple lines")
-print("2. Vertical flair")
-print("3. Emoji sandwich")
 
-style = input("Enter 1, 2 or 3: ").strip()
+updated_words = []
+# process each word
+for word in message.split():
+    cleaned = word.lower().strip(".,!?")
+    emoji = emoji_map_fun.get(cleaned, "")
+    if emoji:
+        updated_words.append(f"{emoji}")
+    else:
+        updated_words.append(word)
 
-def generate_bio(style):
-    if style == "1":
-        return f"{emoji} {name} | {profession} \n💡{passion}\n {website}" 
-    elif style == "2":
-        return f"{emoji} {name}\n{profession}🔥\n{passion}\n{website}🔥"
-    elif style =="3":
-        return f"{emoji*3}\n{name} - {profession}\n {passion}\n{website}\n{emoji}"
-
-bio = generate_bio(style)
-
-print("\nYour stylish bio:\n")
-print("*" * 50)
-print(textwrap.dedent(bio))
-print("*" * 50)
-
-save = input("Do you want to save this bio to a text file? (y/n): ").lower()
-if save == 'y':
-    filename = f"{name.lower().replace(' ', '_')}_bio.txt"
-    with open(filename, "w", encoding="utf-8") as f:
-        f.write(bio)
-    print("file saved")
+updated_message = " ".join(updated_words)
+print(updated_message)
