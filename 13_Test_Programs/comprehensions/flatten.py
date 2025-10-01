3a3a0271c30b36bd8ea1351726fe42c1454424c7
qwerty-arun <arunkr.1804@gmail.com>
2025-10-01 19:15:26 +0530

    restructured multi-threading, multi-processing and GIL files

--- 13_Test_Programs/comprehensions/flatten.py
+++ 13_Test_Programs/comprehensions/flatten.py
@@ -1,5 +1,6 @@
 # Flatten a nested list of integers using list comprehension
 
+# One level flattening
 def flatten(nest):
     flattend = []
     for li in nest:
@@ -10,8 +11,25 @@ def flatten(nest):
             flattend.append(li)
     return flattend
 
+# one level
 def flatten1(nest):
     return [item for li in nest for item in (li if isinstance(li, list) else [li])]
 
+
+# any no. of levels (recursion)
+def flatten2(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten2(item))  # Recursive call
+        else:
+            result.append(item)
+    return result
+
+
 print(flatten([1, [2,3], [4,5,6]]))
+print(flatten([1, [2,3], [4,5,6, [7, 8]]])) # wont work
 print(flatten1([1, [2,3], [4,5,6]]))
+print(flatten1([1, [2,3], [4,5,6, [7, 8]]])) # wont work
+print(flatten2([1, [2,3], [4,5,6]]))
+print(flatten2([1, [2,3], [4,5,6, [7, 8]]])) # oh yes
